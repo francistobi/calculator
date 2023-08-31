da24708c6215f5003dbc4fde8a049e51cf3c5929
francistobi <frobigwe305@gmail.com>
2023-08-31 15:18:52 -0700

    new commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Claculator = require("./classes/calClass");
 
 var val = sqrt(4);
 var cube = cubeRoot(27);
+let Cla = new Claculator("ebuka");
+Claculator.ad;
 
 console.log(val);
 console.log(cube);

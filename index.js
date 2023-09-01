bf195a8dcb1cd6af562499193ae4ba31e92699d6
francistobi <frobigwe305@gmail.com>
2023-09-01 12:52:02 -0700

    new commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var sqrt = require("math-sqrt");
 var cubeRoot = require("cube-root");
-const Claculator = require("./classes/calClass");
+const { Claculator } = require("./classes/calClass");
+const { digitalRobot } = require("./classes/digiRobot");
 
 var val = sqrt(4);
 var cube = cubeRoot(27);
-let Cla = new Claculator("ebuka");
-Claculator.ad;
+let claculator = new Claculator("ebuka");
+var addRobot = digitalRobot(3456);
 
 console.log(val);
 console.log(cube);
+console.log(addRobot);
+console.log(claculator.addition(1, 2, 3, 4));
